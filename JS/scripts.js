cedde1dc229458c398d566079a747e8aa5014df9
albert <albert.cymbaluk@gmail.com>
2017-07-06 12:23:17 +0200

    start project

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+// nowe komantarze są w modzie
+var name = prompt('Enter your name');
+// (lwyłączenie alertu) alert('Hello, ' + name);
+console.log('Hello, ' + name);
+/*
+wystawianie komentarzy 
+i nie tylko
+*/
